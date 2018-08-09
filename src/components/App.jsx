974d0a751b5a1e7e1eeb74e5ab736892447a7cb1
Sebastian  Valiente <svaliente@ellisdon.com>
2018-08-09 16:59:35 -0400

    [master][redis-config] configured redis client to store user info so as to keep track of an okta session even when user closes browser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import React, { Component } from 'react';
-import fetch from 'node-fetch';
+import React, { Component }               from 'react';
+import fetch                              from 'node-fetch';
 
-const client_id      = "0oafsm2mlaNbnoSq40h7"
-const client_secret  = "rHJyWHf7_0pM-qe_Hvw4RD1XhT2LlQtgyy33ElSa"
-const apikey         = "00Aw3z05lA4zZfuKckAMdpZrQ5AJ0StqPXG5CN0tP0"
-const baseUrl        = 'https://dev-957770.oktapreview.com'
-const redirect_uri   = "http://localhost:8080/authorization-code/callback"
+const client_id    = '0oafsm2mlaNbnoSq40h7'
+const baseUrl      = 'https://dev-957770.oktapreview.com'
+const redirect_uri = 'http://localhost:8080/authorization-code/callback'
+const OktaAuth     = require('@okta/okta-auth-js');
+const axios        = require('axios')
 
-const OktaAuth = require('@okta/okta-auth-js');
 const config = {
   url: `${baseUrl}`,
   clientId: `${client_id}`,
@@ -19,6 +18,19 @@ const authClient = new OktaAuth(config)
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {}
+  }
+
+  componentDidMount = async () => {
+    let currentSession = await authClient.session.get()
+    if (currentSession.status == 'ACTIVE') {
+      axios.post('/sessions', {sessionId: currentSession.id})
+      this.setState({currentSession: 'ACTIVE'})
+    } else if (currentSession.status == 'INACTIVE') {
+      let storedSession = await axios.get('/sessions')
+      this.setState({currentSession: storedSession.data.status})
+    }
+
   }
 
   login = () => {
@@ -26,49 +38,33 @@ class App extends Component {
       username: 'conner.mckenna94@gmail.com',
       password: 'Ellisdon2017'
     })
-    .then( async (transaction) => {
-      if (transaction.status == 'SUCCESS') {
+    .then( (transaction) => {
+      if (transaction.status == 'SUCCESS')
         authClient.session.setCookieAndRedirect(transaction.sessionToken, 'http://localhost:8080/login');
-      } else {
+      else
         console.log('login failed')
-      }
+    })
+    .fail( (err) => {
+      console.error("error:", err)
     })
   }
 
   logout = () => {
-    authClient.signOut()
-    .then(function() {
-      console.log('successfully logged out');
-    })
-    .fail(function(err) {
-      console.error(err);
-    });
-
+    axios.delete('/sessions')
+    window.location.reload()
   }
 
-  checkForOktaSession = () => {
-    authClient.session.get()
-    .then( async (session) => {
-      console.log(session)
-    })
-    .catch( (err) => {
-      console.log(err)
-    });
-  }
-
-  checkExpressSession = () => {
-    fetch('/check', {credentials: 'same-origin', mode: 'cors'})
+  checkForOktaSession = async () => {
+    let storedSession = await axios.get('/sessions')
+    console.log(storedSession.data)
+    return storedSession.data
   }
 
   render() {
     return (
       <div>
-        <div>
-          <button onClick={this.login}>LOGIN</button>
-          <a href='/login'> Log </a>
-        </div>
+        <button onClick={this.login}>LOGIN</button>
         <button onClick={this.checkForOktaSession}>CHECK SESSION</button>
-        <button onClick={this.checkExpressSession}>CHECK EXPRESS SESSION</button>
         <button onClick={this.logout}>LOGOUT</button>
       </div>
     )
